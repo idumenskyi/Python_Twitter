e463345062cbf63fea4330eb2b56c6c12e437a2e
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-02-09 19:02:47 +0800

    Create posting_tweet.py

---
+++ posting_tweet.py
@@ -0,0 +1,19 @@
+import tweepy
+
+consumer_key = ''
+consumer_secret = ''
+access_token = '-9XP1LJSljym5ueIuVIkwbBcGwmJuVg'
+access_token_secret = ''
+
+def OAuth():
+	try:
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
+		return auth
+	except Exception as e:
+		return None
+
+oauth = OAuth()
+api = tweepy.API(oauth)
+
+api.update_status('A tweet')
